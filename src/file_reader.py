c4db86de254ff7f4d92ba4d15e3f6373adafe7d4
Yuanhe Tian <yhtian@uw.edu>
2018-04-25 17:23:59 -0700

    add data and fix code

--- src/file_reader.py
+++ src/file_reader.py
@@ -7,31 +7,33 @@
 """
 
 import operator
+import re
 import nltk
 
 data_path = '../data/'
 
-def read_OCR_file(filename):
+def read_file(filename):
     """
-        Read all contents in file
-        :return: list of strings, one string per line
+    Read all contents in file
+    :return: list of strings, one string per line
     """
     import codecs
     with codecs.open(data_path + filename, "r", encoding='utf-8', errors='ignore') as input_file:
         contents = input_file.read()
-        contents = contents.split('\r\n')
+        contents = contents.split('\n')
     return contents
 
 
 def clean_empty_line(raw_lines):
-    raw_content_no_empty_lines = []
+    no_empty_lines = []
     for raw_line in raw_lines:
         raw_line = raw_line.strip()
+        raw_line = re.sub("\\s+", " ", raw_line)
         if operator.eq(raw_line, ""):
             continue
         else:
-            raw_content_no_empty_lines.append(raw_line)
-    return raw_content_no_empty_lines
+            no_empty_lines.append(raw_line)
+    return no_empty_lines
 
 
 def lines2string(lines):
@@ -42,11 +44,21 @@ def lines2string(lines):
     return content_string
 
 
+def get_pairs(ocr_file, gold_standard):
+    raw_content = read_file('OCR_text/newberry-mary-b-some-further-accounts-of-the-nile-1912-1913.txt')
+    gold_content = read_file('gold_standard/Edit_ Some Further Accounts of the Nile 1912-1913.txt')
+    r_content = clean_empty_line(raw_content)
+    g_content = clean_empty_line(gold_content)
+
+    return (lines2string(r_content), lines2string(g_content))
+
 # Test script
 
 if __name__ == "__main__":
-    raw_content = read_OCR_file('newberry-mary-b-some-further-accounts-of-the-nile-1912-1913.txt')
-    content =  clean_empty_line(raw_content)
+    raw_content = read_file('OCR_text/newberry-mary-b-some-further-accounts-of-the-nile-1912-1913.txt')
+    gold_content = read_file('gold_standard/Edit_ Some Further Accounts of the Nile 1912-1913.txt')
+    r_content = clean_empty_line(raw_content)
+    g_content = clean_empty_line(gold_content)
     # print(content)
     '''
     for item in content:
@@ -55,5 +67,6 @@ if __name__ == "__main__":
         print(tokens)
         print("")
     '''
-    clean_string = lines2string(content)
+    r_clean_string = lines2string(r_content)
+    g_clean_string = lines2string(g_content)
     print("hello")
