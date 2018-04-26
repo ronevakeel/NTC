3d2c6799b71f9cd4be52f0ef6f31ff5b28fc5d5c
Yuanhe Tian <yhtian@uw.edu>
2018-04-25 17:59:02 -0700

    add comments to the code

--- src/file_reader.py
+++ src/file_reader.py
@@ -8,13 +8,16 @@
 
 import operator
 import re
-import nltk
 
 data_path = '../data/'
+raw_text_file = "OCR_text/newberry-mary-b-some-further-accounts-of-the-nile-1912-1913.txt"
+gold_text_file = "gold_standard/Edit_ Some Further Accounts of the Nile 1912-1913.txt"
 
 def read_file(filename):
     """
-    Read all contents in file
+    Read all contents in a file, and get a list of lines
+    :type: filename: str
+    :param: filename: name of the file
     :return: list of strings, one string per line
     """
     import codecs
@@ -25,6 +28,12 @@ def read_file(filename):
 
 
 def clean_empty_line(raw_lines):
+    """
+    Clean all empty lines in the list and replace all whitespace character (\t\r\n\f) with a space
+    :type raw_lines: list
+    :param raw_lines: A list of strings need to be cleaned
+    :return: a list of non-empty strings which can be split into tokens by space
+    """
     no_empty_lines = []
     for raw_line in raw_lines:
         raw_line = raw_line.strip()
@@ -37,6 +46,12 @@ def clean_empty_line(raw_lines):
 
 
 def lines2string(lines):
+    """
+    Connect all strings in lines by a space
+    :type lines: str
+    :param lines: A list of string to be connected
+    :return: A string
+    """
     content_string = ""
     for line in lines:
         content_string += line + " "
@@ -44,19 +59,25 @@ def lines2string(lines):
     return content_string
 
 
-def get_pairs(ocr_file, gold_standard):
-    raw_content = read_file('OCR_text/newberry-mary-b-some-further-accounts-of-the-nile-1912-1913.txt')
-    gold_content = read_file('gold_standard/Edit_ Some Further Accounts of the Nile 1912-1913.txt')
+def get_pairs(ocr_file=raw_text_file, gold_standard=gold_text_file):
+    """
+    :type ocr_file: str
+    :param ocr_file: path of raw oct text file, the default is "OCR_text/newberry-mary-b-some-further-accounts-of-the-nile-1912-1913.txt"
+    :type gold_standard: str
+    :param gold_standard: path of the gold standard file, the default is "gold_standard/Edit_ Some Further Accounts of the Nile 1912-1913.txt"
+    :return: A tup whose first element is a string of raw text, and second element is a string of gold standard
+    """
+    raw_content = read_file(ocr_file)
+    gold_content = read_file(gold_standard)
     r_content = clean_empty_line(raw_content)
     g_content = clean_empty_line(gold_content)
-
     return (lines2string(r_content), lines2string(g_content))
 
 # Test script
 
 if __name__ == "__main__":
     raw_content = read_file('OCR_text/newberry-mary-b-some-further-accounts-of-the-nile-1912-1913.txt')
-    gold_content = read_file('gold_standard/Edit_ Some Further Accounts of the Nile 1912-1913.txt')
+    gold_content = read_file('')
     r_content = clean_empty_line(raw_content)
     g_content = clean_empty_line(gold_content)
     # print(content)
