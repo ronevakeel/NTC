3679e949898518692b2e699b835ab3086a1b7b13
Haobo Gu <haobogu@outlook.com>
2018-04-25 18:33:58 -0700

    Correct WER evaluation, remove rouge evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,15 +1,18 @@
 """
 Provides evaluation methods for OCR correction result.
+
+    Author: Haobo Gu
+    Date created: 04/24/2018
+    Python version: 3.6.2
 """
 import nltk
 import src.wer as wer
-import pyrouge
 
 
 def evaluate(result_string, gold_string, eval_method='wer'):
     """
-    Calculate overall word match rate between result string and gold string.
-    WMR is an evaluation measurement with considering order of the word in the sentence
+    Evaluate system's performance using word error rate or rouge measurement.
+    Use 'wer' and 'rouge' to specify which measurement you're going to use.
     :type result_string: str
     :type gold_string: str
     :type eval_method: str
@@ -20,35 +23,29 @@ def evaluate(result_string, gold_string, eval_method='wer'):
         ref_list = gold_string.split()
         # print(word_list1, word_list2)
         eval_result = wer.calculate_wer(ref_list, hyp_list)
-    # elif eval_method == 'rouge':
-    #     # pyrouge.Rouge155.convert_summaries_to_rouge_format(system_input_dir, system_output_dir)
-    #     return
+
     return eval_result
 
-def evaluate_file(hyp_filename, ref_filename, eval_method='wer'):
-    """
-    Calculate overall word match rate between result string and gold string.
-    WMR is an evaluation measurement with considering order of the word in the sentence
-    :type result_string: str
-    :type gold_string: str
-    :type eval_method: str
-    :rtype: float
-    """
-    if eval_method == 'wer':
-        return 0
-        # hyp_list = result_string.split()
-        # ref_list = gold_string.split()
-        # # print(word_list1, word_list2)
-        # eval_result = wer.calculate_wer(ref_list, hyp_list)
-    elif eval_method == 'rouge':
-        r = pyrouge.Rouge155()
-        r.system_dir = 'path/to/system_summaries'
-        r.model_dir = 'path/to/model_summaries'
-        r.system_filename_pattern = 'some_name.(\d+).txt'
-        r.model_filename_pattern = 'some_name.[A-Z].#ID#.txt'
 
-        eval_result = r.convert_and_evaluate(hyp_filename, ref_filename)
-    return eval_result
+# def evaluate_by_folder(result_folder, gold_folder, result_name_pattern, gold_name_pattern, eval_method='rouge'):
+#     """
+#     Evaluate system's performance using word error rate or rouge measurement.
+#     All files in the folder will be read.
+#     Use 'wer' and 'rouge' to specify which measurement you're going to use.
+#     :type result_folder: str
+#     :type gold_folder: str
+#     :type eval_method: str
+#     :rtype: dictionary
+#     """
+#     if eval_method == 'wer':
+#         return 0
+#         # hyp_list = result_string.split()
+#         # ref_list = gold_string.split()
+#         # # print(word_list1, word_list2)
+#         # eval_result = wer.calculate_wer(ref_list, hyp_list)
+
+#     return eval_result
+
 
 
 """
@@ -77,5 +74,6 @@ def word_match_rate(result_string, gold_string, scope=10):
 # Test script
 if __name__ == '__main__':
     hyp = 'This machine can wreck a nice beach'
-    ref = 'This great machine can recognize speech'
-    print(evaluate(hyp, ref))
+    # ref = 'This great machine can recognize speech'
+    ref = 'This machine can wreck a good beach'
+    print(evaluate(hyp, ref, 'wer'))
